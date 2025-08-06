243010f1751c4ff90025d5f5f7a9e19a761c8575
root <root@RAKAZ-HEV2.localdomain>
2025-08-06 20:34:28 +0300

    both server and client are working

--- svc.py
+++ svc.py
@@ -3,9 +3,8 @@ import socket
 import os
 
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-my_socket.connect(('192.168.68.118', 1729))
-
-file = input("Please enter a file path, either absolute or relative to to Desktop \n")
+my_socket.connect(('172.27.167.156.', 1730))
+file = input("Please enter a file path, either absolute or relative to simple-server \n")
 fileName = input("please name the file (include type) \n")
 my_socket.send(str(len(fileName)).zfill(2).encode())
 my_socket.send(fileName.encode())
